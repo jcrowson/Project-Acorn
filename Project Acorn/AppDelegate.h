ae90241bb0806edfd0e658a1d837e2c85e210567
James Crowson <crowson.j@gmail.com>
2012-09-18 22:24:36 +0100

    Initial commit
    Added the scaffold project.

---
+++ Project Acorn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project Acorn
+//
+//  Created by James Crowson on 18/09/2012.
+//  Copyright (c) 2012 James Crowson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project Acorn/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Project Acorn
+//
+//  Created by James Crowson on 18/09/2012.
+//  Copyright (c) 2012 James Crowson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
